c0a38883320631810b8a5a4d9cab92cd79fd4f14
Dmitry Morsky <dmitry.morsky@gmail.com>
2016-05-24 21:08:24 +0600

    Full commit for Schedule task

---
+++ Morsky.Nsudotnet.Schedule/Morsky.Nsudotnet.Schedule/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morsky.Nsudotnet.Schedule
+{
+    class MyJob1 : IJob
+    {
+        public void Execute(object argument)
+        {
+            System.Console.WriteLine("First!");
+        }
+    }
+
+    class MyJob2 : IJob
+    {
+        public void Execute(object argument)
+        {
+            System.Console.WriteLine("Second!");
+        }
+    }
+
+    class MyJob3 : IJob
+    {
+        public void Execute(object argument)
+        {
+            System.Console.WriteLine("Third!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schedule scheldure = new Schedule();
+            scheldure.ScheduleDelayedJob(new MyJob1(), TimeSpan.FromMilliseconds(2000), null);
+            scheldure.SchedulePeriodicJob(new MyJob2(), TimeSpan.FromMilliseconds(3000), null);
+            scheldure.SchedulePeriodicJob(new MyJob3(), TimeSpan.FromMilliseconds(1000), null);
+            Console.ReadKey();
+        }
+    }
+}
